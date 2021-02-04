e7d9be437a754ef5b50197c8040593c1de758820
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2021-02-05 02:34:26 +0700

    feat: add register feature

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, render_template, jsonify
 from flaskext.mysql import MySQL
-import pymysql
 from werkzeug.security import generate_password_hash, check_password_hash
 from models.user import User
 
@@ -14,11 +13,10 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 db.init_app(app)
 
 conn = db.connect()
-cursor = conn.cursor(pymysql.cursors.DictCursor)
 
 @app.route('/login', methods=['POST'])
 def login():
-  user = User(cursor=cursor).find_by_username(request.form['username'])
+  user = User(conn=conn).fetch(request.form['email'])
   if user and check_password_hash(user['password'], request.form['password']):
     # TODO: redirect to home, add session
     return jsonify({'message': 'Password is correct'})
@@ -26,9 +24,27 @@ def login():
   return jsonify({'error': 'User or password are incorrect'}), 401
 
 
+@app.route('/register', methods=['POST'])
+def register():
+  user = User(conn=conn).fetch(request.form['email'])
+  if user:
+    return jsonify({'message': 'Email has been registered, please proceed to login'}), 400
+  else:
+    if User(
+      firstname=request.form['firstname'],
+      lastname=request.form['lastname'],
+      email=request.form['email'],
+      password=generate_password_hash(request.form['password']),
+      conn=conn
+    ).create(): return jsonify({'message': 'User has registered successfully'})
+
 @app.route('/')
 def index():
   return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+  return render_template('signup.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
--- models/user.py
+++ models/user.py
@@ -1,9 +1,24 @@
-class User:
-  def __init__(self, username=None, password=None, cursor=None):
-    self.username = username
-    self.password = password
-    self.cursor = cursor
+import pymysql
 
-  def find_by_username(self, username):
-    data = self.cursor.execute('SELECT * FROM users WHERE username = %s', (username))
-    return dict(self.cursor.fetchone())
+class User:
+  def __init__(self, firstname=None, lastname=None, email=None, password=None, conn=None):
+    self.firstname = firstname
+    self.lastname = lastname
+    self.email = email
+    self.password = password
+    self.conn = conn
+    self.cursor = self.conn.cursor(pymysql.cursors.DictCursor)
+
+
+  def create(self):
+    self.cursor.execute(
+      'INSERT INTO users VALUES (NULL, %s, %s, %s, %s)', 
+      (self.firstname, self.lastname, self.email, self.password)
+    )
+    self.conn.commit()
+    return self.fetch(self.email)
+    
+    
+  def fetch(self, email):
+    data = self.cursor.execute('SELECT * FROM users WHERE email = %s', (email))
+    return self.cursor.fetchone()
