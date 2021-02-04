3f20670ab4009b5e49ab6311298ecc8cc140668d
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2021-02-05 01:37:29 +0700

    feat: add login functionality

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from flaskext.mysql import MySQL
 import pymysql
+from werkzeug.security import generate_password_hash, check_password_hash
+from models.user import User
 
 app = Flask(__name__)
 
-
-# refactor needed
-# db connection should be created as a class on a seperated file
 db = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'librari'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 db.init_app(app)
+
+conn = db.connect()
+cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+@app.route('/login', methods=['POST'])
+def login():
+  user = User(cursor=cursor).find_by_username(request.form['username'])
+  if user and check_password_hash(user['password'], request.form['password']):
+    # TODO: redirect to home, add session
+    return jsonify({'message': 'Password is correct'})
+  
+  return jsonify({'error': 'User or password are incorrect'}), 401
+
 
 @app.route('/')
 def index():
