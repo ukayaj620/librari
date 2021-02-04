86d360a00e6a9a8d69985b515ccbb6a0063ca503
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2021-02-05 00:24:50 +0700

    feat: render sign in page, instantiate db

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from flaskext.mysql import MySQL
+import pymysql
 
 app = Flask(__name__)
 
+
+# refactor needed
+# db connection should be created as a class on a seperated file
+db = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'librari'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+db.init_app(app)
+
 @app.route('/')
 def index():
-  return '<h1>Hello World</h1>'
+  return render_template('index.html')
+
+if __name__ == '__main__':
+  app.run(debug=True)
