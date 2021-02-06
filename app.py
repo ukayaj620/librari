a567c7e7c31cdf03dbb977eca24a6a8d4c30806e
ukayaj620 <51906567+ukayaj620@users.noreply.github.com>
2021-02-07 03:07:22 +0700

    feat: finish simple authentication

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify, session, redirect, url_for
 from flaskext.mysql import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
 from models.user import User
+import credentials
 
 app = Flask(__name__)
+app.secret_key = credentials.SECRET_KEY
 
 db = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'librari'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = credentials.DB_USERNAME
+app.config['MYSQL_DATABASE_PASSWORD'] = credentials.DB_PASSWORD
+app.config['MYSQL_DATABASE_DB'] = credentials.DB_DBNAME
+app.config['MYSQL_DATABASE_HOST'] = credentials.DB_HOSTNAME
 db.init_app(app)
 
 conn = db.connect()
@@ -18,10 +20,19 @@ conn = db.connect()
 def login():
   user = User(conn=conn).fetch(request.form['email'])
   if user and check_password_hash(user['password'], request.form['password']):
-    # TODO: redirect to home, add session
-    return jsonify({'message': 'Password is correct'})
+    session['name'] = user['firstname'] + ' ' + user['lastname']
+    session['email'] = user['email']
+    session['loggedIn'] = True
+    return redirect(url_for('index'))
   
   return jsonify({'error': 'User or password are incorrect'}), 401
+
+@app.route('/logout', methods=['GET'])
+def logout():
+  session.pop('name', None)
+  session.pop('email', None)
+  session.pop('loggedIn', None)
+  return redirect(url_for('index'))
 
 
 @app.route('/register', methods=['POST'])
@@ -40,11 +51,14 @@ def register():
 
 @app.route('/')
 def index():
+  if 'loggedIn' in session and session['loggedIn'] == True:
+    return render_template('home.html')
   return render_template('index.html')
 
 @app.route('/signup')
 def signup():
   return render_template('signup.html')
+
 
 if __name__ == '__main__':
   app.run(debug=True)
